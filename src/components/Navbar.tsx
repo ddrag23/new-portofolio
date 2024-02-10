0346ce6c65a1aefdb20182657aafea807d54911c
ddrag23 <kurodyami@gmail.com>
2024-02-11 02:37:29 +0700

    :hammer: add: button in home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
 "use client"
 import { NAVBAR_ITEMS } from "@/constant/navbar";
 import clsx from "clsx";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 export default function Navbar() {
     const pathname = usePathname()
     return <>
-        <nav className="w-full py-5 px-5 md:px-10 md:flex justify-between font-mono fixed z-10 bg-white hidden">
-            <Link href="/" className="font-bold">Mohammad Baidhowi Alwi</Link>
-            <div className="md:flex md:gap-3 hidden">
+        <nav className="w-full py-5 px-5 md:px-10 md:flex justify-between items-center font-mono fixed z-10 bg-white hidden">
+            <Link href="/" className="font-bold flex items-center gap-2"><Image
+                width={30} height={30} alt="profile" src="/icon/js.png" className="rounded-full" /><span>Mohammad Baidhowi Alwi</span></Link>
+            <div className="md:flex md:gap-3 hidden items-center">
                 {NAVBAR_ITEMS.map((item, key) => {
                     const isActivePage = pathname == item.pathname
                     return <Link href={item.pathname} key={key} className={clsx(isActivePage && "font-bold")}>{item.title}</Link>
--- src/components/Personal.tsx
+++ src/components/Personal.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Personal() {
     return <>
         <div className="flex items-center content-center justify-center flex-col h-full fixed w-full">
             <h1 className="text-4xl font-bold mx-5">Welcome To My Website</h1>
             <p className="mx-5">I'm a software engineer. I'm used to using Laravel and Vuejs for web development</p>
+            <div className="mt-3 flex gap-3 flex-col md:flex-row w-full justify-center">
+                <Link className="bg-slate-800 px-5 py-2 rounded-md text-white mx-5 md:mx-0" href="/project">Explore My Project</Link>
+                <Link className="bg-slate-500 px-5 py-2 rounded-md text-white mx-5 md:mx-0" href="/contact">Contact Me</Link>
+            </div>
         </div>
     </>
 }
